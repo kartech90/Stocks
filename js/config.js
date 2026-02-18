af89c050fa6b603e936171c84025d4f318c7989b
kartech90 <lk@pomkosher.com>
2026-02-18 14:17:22 -0500

    Add JPM and NET to stock symbols
    Added new stock symbols 'JPM' and 'NET' to the STOCK_SYMBOLS array.

--- js/config.js
+++ js/config.js
@@ -4,12 +4,10 @@ const LOGO_EXT = ".png";
 const STOCK_SYMBOLS = [
     "SHOP", "HOOD", "AAPL", "MSFT", "GOOG", "TSLA",
     "PYPL", "WMT", "BABA", "RIVN", "AMZN", "XIACY",
-    "APP", "META", "NFLX", "SOFI", "NVDA", "SPY", "BTC"
+    "APP", "META", "NFLX", "SOFI", "NVDA", "SPY", "BTC", "JPM", "NET", "QCOM"
 ];
 
 const STOCKS = STOCK_SYMBOLS.map(s => [s, `${LOGO_PATH}${s}${LOGO_EXT}`]);
 
 const BOARD_SIZE = 6;
-
 const API_KEY = "d5qqfa1r01qhn30ha6k0d5qqfa1r01qhn30ha6kg";
-
