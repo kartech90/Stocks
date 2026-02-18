b5c649fa67dd5a3b1cc81dbcaef13357ac431237
kartech90 <lk@pomkosher.com>
2026-02-18 12:40:12 -0500

    Add 'SPY' to STOCK_SYMBOLS array

--- js/config.js
+++ js/config.js
@@ -4,10 +4,11 @@ const LOGO_EXT = ".png";
 const STOCK_SYMBOLS = [
     "SHOP", "HOOD", "AAPL", "MSFT", "GOOG", "TSLA",
     "PYPL", "WMT", "BABA", "RIVN", "AMZN", "XIACY",
-    "APP", "META", "NFLX", "SOFI", "NVDA"
+    "APP", "META", "NFLX", "SOFI", "NVDA", "SPY"
 ];
 
 const STOCKS = STOCK_SYMBOLS.map(s => [s, `${LOGO_PATH}${s}${LOGO_EXT}`]);
 
 const BOARD_SIZE = 6;
+
 const API_KEY = "d5qqfa1r01qhn30ha6k0d5qqfa1r01qhn30ha6kg";
