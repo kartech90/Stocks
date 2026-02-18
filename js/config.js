71372fd9e91d6f8d1c36757842c629a32c663c15
kartech90 <lk@pomkosher.com>
2026-02-18 12:59:17 -0500

    Add 'BTC' to STOCK_SYMBOLS in config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const LOGO_EXT = ".png";
 const STOCK_SYMBOLS = [
     "SHOP", "HOOD", "AAPL", "MSFT", "GOOG", "TSLA",
     "PYPL", "WMT", "BABA", "RIVN", "AMZN", "XIACY",
-    "APP", "META", "NFLX", "SOFI", "NVDA", "SPY"
+    "APP", "META", "NFLX", "SOFI", "NVDA", "SPY", "BTC"
 ];
 
 const STOCKS = STOCK_SYMBOLS.map(s => [s, `${LOGO_PATH}${s}${LOGO_EXT}`]);
@@ -12,3 +12,4 @@ const STOCKS = STOCK_SYMBOLS.map(s => [s, `${LOGO_PATH}${s}${LOGO_EXT}`]);
 const BOARD_SIZE = 6;
 
 const API_KEY = "d5qqfa1r01qhn30ha6k0d5qqfa1r01qhn30ha6kg";
+
